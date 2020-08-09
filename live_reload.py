d301b8996d244bd6f08c91e3e01d8f084b74d099
aashu.dwivedi@gmail.com <ashutosh.dwivedi@freshworks.com>
2020-08-09 15:14:23 +0530

    added comment in live_reload

--- live_reload.py
+++ live_reload.py
@@ -2,5 +2,6 @@
 from livereload import Server, shell
 
 server = Server()
+# -t defines the theme location while generating pelican content
 server.watch('themes/', shell('pelican content -s pelicanconf.py -t ./themes/basic', cwd='.'))
 server.serve(root='output')
